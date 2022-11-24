4c0bc209a48bb0966ea9a37e37e95f6a683e409b
Maanav Rajesh <65039621+maanav13@users.noreply.github.com>
2022-11-24 17:24:57 -0500

    edit schema and routes (#22)

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    _id:        {type: Number},
     username:   {type: String,
                 required: true,
                 unique: true,
