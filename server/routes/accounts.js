3563ac937291c5be1a0eea3d094e5e79dbf32f94
Maanav Rajesh <maanavrajesh13@gmail.com>
2022-11-24 21:16:25 -0500

    Fix accounts routes

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -3,20 +3,26 @@ let Account = require('../models/Account');
 var mongoose = require('mongoose');
 var mongodb = require('mongodb')
 
-// router.route('/').get((req, res) => {
-//   Account.where()
-//     .then(accounts => res.json(accounts))
-//     .catch(err => res.status(400).json('Error: ' + err));
-//   });
+ router.route('/').get((req, res) => {
+  Account.where()
+     .then(accounts => res.json(accounts))
+     .catch(err => res.status(400).json('Error: ' + err));
+    });
 
-router.route('/:id').get((req, res) => {
-    Account.findById(req.params.id)
+// router.route('/:id').get((req, res) => {
+//     Account.findById(req.params.id)
+//       .then(accounts => res.json(accounts))
+//       .catch(err => res.status(400).json('Error: ' + err));
+//     });
+
+router.route('/:userID/:accountID').get((req, res) => {
+    Account.where({userID: req.params.userID, _id: mongoose.Types.ObjectId.createFromHexString(req.params.accountID)})
       .then(accounts => res.json(accounts))
       .catch(err => res.status(400).json('Error: ' + err));
     });
 
-router.route('/:userID/:accountID').get((req, res) => {
-    Account.where({userID: mongoose.Types.ObjectId.createFromHexString(req.params.userID), _id: mongoose.Types.ObjectId.createFromHexString(req.params.accountID)})
+router.route('/:userID').get((req, res) => {
+    Account.where({userID: req.params.userID})
       .then(accounts => res.json(accounts))
       .catch(err => res.status(400).json('Error: ' + err));
     });
