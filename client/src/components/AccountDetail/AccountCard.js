808d0ede622732604851e459b535e8c9d87eb322
Preeti <preetivalunjkar1540@gmail.com>
2022-12-01 11:54:05 -0500

    Changes to card colours

--- client/src/components/AccountDetail/AccountCard.js
+++ client/src/components/AccountDetail/AccountCard.js
@@ -4,35 +4,37 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import logo from "../../assets/BANKLOGO.png"
 
 export default function AccountCard({type, balance, accountId}) {
+  const upperacc = accountId.toUpperCase();
   return (
-    <Card style={{backgroundColor: 'rgba(152, 255, 152, 0.5)'}} sx={{ minWidth: 275 }}>
+    <Card style={{backgroundColor: "teal"}} sx={{ minWidth: 260, minHeight: 225 }}>
       <CardContent>
         <table width="100%">
           <tbody>
             <tr>
               <td>
-                <Typography variant="h5" component="div">
+                <Typography variant="h5" component="div" style={{color: "white", fontSize: '21px'}}>
                   {type}
                 </Typography>
               </td>
               <td>
-                <Typography align="right" variant="h5" component="div">
+                <Typography align="right" variant="h5" component="div" style={{color: "white", fontSize: '25px'}}>
                   $ {balance}
                 </Typography>
               </td>
             </tr>
           </tbody>
         </table>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          {accountId}
+        <Typography sx={{ mb: 1.5 }} color="text.secondary" style={{color: "white", fontSize: '15px'}}>
+          {upperacc}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button variant="contained" style={{backgroundColor: "teal"}} size="small">Next</Button>
+        <Button variant="contained" style={{backgroundColor: "teal", top: 78, left: 10}} size="small">Details</Button>
       </CardActions>
+      <img src={logo} alt="Bank Logo" width={275} height={100} align="right" />
     </Card>
   );
 }
